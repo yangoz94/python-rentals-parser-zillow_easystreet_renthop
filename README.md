d4519d577b39ed12d6ba764012ab8ab9ebc26570
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-06-13 18:23:43 -0400

    replace the quickstart.py with sheets.py to get all rows and add a new row

--- README.md
+++
@@ -1,2 +0,0 @@
-# Parser for listings on zillow, easystreet, renthop.com 
-Parses and adds the extracted attributes to the specified google sheets document
--- utilities.py
+++
@@ -1,6 +0,0 @@
-def extract_renthop_availability(text: str) -> str:
-    try:
-        separated_text = text.split(',')[1].replace('Move-In', '').strip()
-        return separated_text
-    except Exception as e:
-        raise Exception('Error extracting availability from RentHop: {}'.format(str(e)))
