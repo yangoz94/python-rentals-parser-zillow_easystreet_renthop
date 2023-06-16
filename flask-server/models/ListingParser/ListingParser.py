621fddc668916371bcbc7bb92c0beda691aac2df
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-06-16 10:01:31 -0400

    add more headers to doge 403:Forbidden error due to blockage on VPS

--- flask-server/models/ListingParser/ListingParser.py
+++ flask-server/models/ListingParser/ListingParser.py
@@ -26,6 +26,9 @@ class ListingParser:
             ]
             headers = {
                 "User-Agent": random.choice(USER_AGENTS),
+                "Accept-Language": "en-US,en;q=0.9",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Referer": self.url
             }
             async with aiohttp.ClientSession() as session:
                 async with session.get(self.url, headers=headers) as response:
